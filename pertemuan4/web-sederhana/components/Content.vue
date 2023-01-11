6dd55cd97bfbd85863c6efef0577ad7e6653a5f9
delshady7 <delshae256@gmail.com>
2023-01-11 16:28:07 +0700

    web

---
+++ pertemuan4/web-sederhana/components/Content.vue
@@ -0,0 +1,40 @@
+<!-- <template>
+<div class="home">
+<div class="judul-content">
+    <h2>Home</h2>
+<div class="isi-content">
+    <div>
+        <h4>Genre Novel</h4>
+        <ul>
+            <li v-for="jns of jenis" :key="jns">{{ jns }}</li>
+        </ul>
+    </div>
+    <div class="button">
+        <button @click="$emit('novel')">Click Me</button>
+        <br><br>
+    </div>
+</div>
+</div>
+</div>
+</template>
+<style>
+.judul-content{
+    margin-top: 20px;
+    margin-left: 20px;
+    font-family: 'Poppins', sans serif;
+    font-weight: 900;;
+    font-size: 20px;
+}
+</style>
+
+<script>
+export default{
+    name: 'Content',
+    data() {
+        return{
+            jenis: ['Romance', 'Slice of life', 'Angst']
+        }
+    },
+};
+
+</script> -->
---
+++ pertemuan4/web-sederhana/pages/web/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="home" style="height: 100vh">
+    <div class="title" style="margin-top: 80px">
+      <center>Dad Jokes</center>
+    </div>
+    <div class="search" style="margin-top: 70px">
+      <v-container>
+        <center>
+          <v-card
+            color="lighten-4"
+            flat
+            height="140px"
+            style="background-color: #d9d9d9"
+          >
+            <v-toolbar>
+              <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+              </v-btn>
+              <v-text-field
+                v-model="sb"
+                placeholder="Search a Dad Jokes"
+                style="border-radius: 56px"
+              ></v-text-field>
+            </v-toolbar>
+          </v-card>
+        </center>
+      </v-container>
+    </div>
+    <div v-if="sb === null || sb === ''">
+      <center>
+        <p>Data not Available</p>
+      </center>
+    </div>
+    <div v-else>
+      <div>
+        <h2>Result: {{ itemResult.length }}</h2>
+      </div>
+      <div v-for="item in itemResult" :key="item">
+        <v-container>
+          <v-card
+            height="120px"
+            style="background-color: #ffff; border-radius: 16px"
+          >
+            <v-card-text>
+              {{ item }}
+            </v-card-text>
+          </v-card>
+          <br />
+        </v-container>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data: () => ({
+    sb: null,
+    itemResult: [
+      "Did you hear the one about the guy with the broken hearing aid? Neither did he.",
+      "Did you hear the one about the guy with the broken hearing aid? Neither did he.",
+      "Did you hear the one about the guy with the broken hearing aid? Neither did he.",
+      "Did you hear the one about the guy with the broken hearing aid? Neither did he.",
+    ],
+  }),
+};
+</script>
+
+<style>
+html {
+  background-color: #d9d9d9;
+}
+.home {
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  background-color: #d9d9d9;
+}
+.home h2 {
+  font-size: 16px;
+  margin-left: 100px;
+}
+.title {
+  font-size: 35px;
+}
+.search {
+  background-color: #d9d9d9;
+}
+</style>
